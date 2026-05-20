853a10ed09282133d8415ffb0a08b96cfb5a3be5
Ian L <187871945+Ehan1213@users.noreply.github.com>
2026-05-19 22:30:13 -0400

    Add Service SQL model and prep for Alembic

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
+import uuid
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -7,3 +11,15 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
+
+
+class Service(db.Model):
+    __tablename__ = "services"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(db.String(50))
+    url: Mapped[str] = mapped_column(db.String(2048))
+    check_interval_seconds: Mapped[int] = mapped_column(default=300)
+    created_at: Mapped[datetime] = mapped_column(server_default=db.func.now())
